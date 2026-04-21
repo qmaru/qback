5134022c19e872939443af82558a4ba8da1e813e
unknown <csmcc2@hotmail.com>
2026-04-21 14:23:29 +0800

    reduce tools

--- utils/tools.go
+++ utils/tools.go
@@ -1,7 +1,8 @@
 package utils
 
 import (
-	"github.com/qmaru/minitools/v2/data/json/gojson"
+	"fmt"
+
 	"github.com/qmaru/minitools/v2/file"
 	"github.com/qmaru/minitools/v2/hashx/blake3"
 )
@@ -9,5 +10,24 @@ import (
 var (
 	FileSuite   = file.New()
 	Blake3Suite = blake3.New()
-	JSONSuite   = gojson.New()
 )
+
+func PrettySize(size int64) string {
+	const unit = 1024
+	if size < unit {
+		return fmt.Sprintf("%d B", size)
+	}
+	div, exp := int64(unit), 0
+	for n := size / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB", float64(size)/float64(div), "KMGTPE"[exp])
+}
+
+func PrettyHash(h string) string {
+	if len(h) <= 8 {
+		return h
+	}
+	return h[:8]
+}
